6e727015b6e72b802972d567d03d45f5d28a88db
Paweł Wojtanowicz <pwojtanowicz@PFEIFER.Polska>
2025-02-20 14:25:43 +0100

    example test modified try connect and  test pfeifer suit

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,5 +1,9 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://test.pfeifer-suite.info/')
+    cy.get('input').first().type('pawel.wojtanowicz@pfeifer.pl')
+    cy.get('input').eq(1).type('abc')
+    cy.contains('Login').click()
+    cy.wait(10000)
   })
 })
